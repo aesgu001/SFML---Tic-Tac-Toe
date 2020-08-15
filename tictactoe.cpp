e1adf6118fb1ac4b63b653ba41f7251adf81be87
Angelo Esguerra <aesguerra972@yahoo.com>
2020-08-15 03:47:48 -0700

    Board displaying in terminal correctly.

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -3,29 +3,42 @@
 Tictactoe::Tictactoe()
 {
 	initBoard();
+	initPlayers();
 }
 
 void Tictactoe::initBoard()
 {
-	for (int i = 0; i < 3; i++)
+	for (size_t i = 0; i < 3; i++)
 	{
 		this->board.push_back(std::vector<char>(3));
 
-		for (int j = 0; j < this->board[i].size(); j++)
+		for (size_t j = 0; j < this->board[i].size(); j++)
 		{
 			this->board[i][j] = static_cast<char>(49 + (i * 3) + j);
 		}
 	}
 }
 
+void Tictactoe::initPlayers()
+{
+	this->player1 = 'X';
+	this->player2 = 'O';
+}
+
 void Tictactoe::displayBoard()
 {
-	for (int i = 0; i < this->board.size(); i++)
+	for (size_t i = 0; i < this->board.size(); i++)
 	{
-		for (int j = 0; j < this->board[i].size(); j++)
+		for (size_t j = 0; j < this->board[i].size(); j++)
 		{
-			std::cout << this->board[i][j] << " ";
+			std::cout << " " << this->board[i][j];
+
+			if (j + 1 < this->board[i].size())
+				std::cout << " |";
 		}
 		std::cout << std::endl;
+
+		if (i + 1 < this->board.size())
+			std::cout << "-----------" << std::endl;
 	}
 }
--- tictactoe.h
+++ tictactoe.h
@@ -7,10 +7,13 @@ class Tictactoe
 {
 private:
 	std::vector<std::vector<char> > board;
+	char player1;
+	char player2;
 
 public:
 	Tictactoe();
 
 	void initBoard();
+	void initPlayers();
 	void displayBoard();
 };
