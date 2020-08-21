69c36600193d97c8464fc07dbb95c97a91dac479
Angelo Esguerra <aesguerra972@yahoo.com>
2020-08-21 06:22:45 -0700

    game window draws a green circle.

--- main.cpp
+++ main.cpp
@@ -1,51 +1,28 @@
 // #include "tictactoe.h"
 
-// #include <SFML/Graphics.hpp>
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
 
 int main()
 {
-    /*sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);*/
+    RenderWindow window(VideoMode(800, 600), "SFML Works!", Style::Titlebar | Style::Close);
+    CircleShape shape(100.f);
+    shape.setFillColor(Color::Green);
 
-    // Tictactoe tTT;
-    // char currentPlayer = '\0';
-
-    /*while (!tTT.gameOver(currentPlayer))
+    while (window.isOpen())
     {
-        /*sf::Event event;
+        Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == Event::Closed)
                 window.close();
         }
 
         window.clear();
         window.draw(shape);
-        window.display();*/
-
-        /*currentPlayer = tTT.getNextPlayer(currentPlayer);
-        
-        tTT.displayBoard();
-        std::cout << "Player " << currentPlayer << "'s turn" << std::endl;
-
-        size_t pos;
-        do
-        {
-            std::cout << "Enter a position <1 - 9>: ";
-            std::cin >> pos;
-        } while (!tTT.setPlayerPos(currentPlayer, pos));
-    }*/
-
-    /*tTT.displayBoard();
-    if (tTT.getTieGame())
-        std::cout << "It's a tie game!" << std::endl;
-    else
-        std::cout << "Player " << currentPlayer << " wins!" << std::endl;
-   
-    char exitKey;
-    std::cout << "Press any key to exit the game: ";
-    std::cin >> exitKey;*/
+        window.display();
+    }
 
     return 0;
 }
